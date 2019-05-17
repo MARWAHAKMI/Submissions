0372a987cb29419286dc283a87f7dc27613e58c1
MARWAHAKMI <marwahakmi@gmail.com>
2019-05-17 03:45:36 +0300

    Step#6

---
+++ Exercises/Javascript-Basics-01/6.js
@@ -0,0 +1,7 @@
+var a = parseFloat(prompt('Enter num 1'));
+var b = parseInt(prompt('Enter num 2'));
+
+var d = a / b;
+var m = a % b;
+
+alert(a + ' Divided by ' + b + ' = ' + d + '\nREmainder is ' + m);
