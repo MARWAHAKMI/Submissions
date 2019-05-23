fe338f391f3078efc9b7d521977f7190c91bbe3b
MARWAHAKMI <marwahakmi@gmail.com>
2019-05-23 11:08:36 +0300

    JS-Basics02-steps#6

---
+++ Exercises/Javascript-Basics-02/step 6/Step_6.js
@@ -0,0 +1,62 @@
+function myFunc(a)
+{
+    switch(a)
+    {
+        case 1:
+        {
+            document.getElementById('image1').src = "images/image1_2.jpg"
+            break 
+        }
+        case 2:
+            document.getElementById('image2').src = "images/image2_2.jpg"
+            break 
+        case 3:
+        {
+            document.getElementById('image3').src = "images/image3_2.jpg"
+            break 
+        }
+        case 4:
+        {
+            document.getElementById('image4').src = "images/image4_2.jpg"
+            break 
+        }
+        case 5:
+        {
+            document.getElementById('image5').src = "images/image5_2.jpg"
+            break 
+        }
+
+    }
+}
+///////////////////////////////////////////
+
+function myFunc2(b)
+{
+    switch(b)
+    {
+        case 1:
+        {
+            document.getElementById('image1').src = "images/image1.jpg"
+            break 
+        }
+        case 2:
+            document.getElementById('image2').src = "images/image2.jpg"
+            break 
+        case 3:
+        {
+            document.getElementById('image3').src = "images/image3.jpg"
+            break 
+        }
+        case 4:
+        {
+            document.getElementById('image4').src = "images/image4.jpg"
+            break 
+        }
+        case 5:
+        {
+            document.getElementById('image5').src = "images/image5.jpg"
+            break 
+        }
+
+    }
+}
