3ff7993d1cff4af96022f44e2eec81d8aa6b1e9c
MARWAHAKMI <marwahakmi@gmail.com>
2019-05-23 11:30:13 +0300

    JS-Basics03-steps#2

---
+++ Exercises/Javascript-Basics-03/step 2/Step_2.js
@@ -0,0 +1,18 @@
+var x = document.querySelector('p')
+function toggle(a)
+{
+    switch(a)
+    {
+        case 1:
+        {
+            x.style.display = "block"
+            break
+        }
+        case 2:
+        {
+            x.style.display = "none"
+            break
+        }
+
+    }
+}
