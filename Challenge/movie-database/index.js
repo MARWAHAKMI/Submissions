f9d26da61a5e3dbe6a60784b6287b51a4db6f02c
MARWAHAKMI <marwahakmi@gmail.com>
2019-06-12 15:50:09 +0300

    movie-step#3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,3 +1,6 @@
+
+/////Step#2///////
+
 var express = require('express');
 var port = 3000 ;
 var app = express();
@@ -6,4 +9,18 @@ app.listen(port , console.log("Server is running Successfully"));
 
 app.get('/' , function(require ,result){
     result.send('ok');
+})
+
+////////////////////////////////////
+
+/////Step#3/////////////
+
+app.get('/test' , function(require ,result){
+    result.send({status:200, message:"ok"});
+})
+
+
+app.get('/time' , function(require ,result){
+    var date = new Date();
+    result.send({status:200, message: date.getHours() + ":" + date.getSeconds()});
 })
