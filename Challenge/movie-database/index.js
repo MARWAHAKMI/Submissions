941e59c5bc6b411c6200bef0c55f5162c360866a
MARWAHAKMI <marwahakmi@gmail.com>
2019-06-12 16:30:41 +0300

    movie-step#4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,26 +1,42 @@
 
 /////Step#2///////
-
 var express = require('express');
 var port = 3000 ;
 var app = express();
 
 app.listen(port , console.log("Server is running Successfully"));   
 
-app.get('/' , function(require ,result){
+app.get('/' , function(request ,result){
     result.send('ok');
 })
-
-////////////////////////////////////
+///////////////////End Step#2/////////////////
 
 /////Step#3/////////////
-
-app.get('/test' , function(require ,result){
+app.get('/test' , function(request ,result){
     result.send({status:200, message:"ok"});
 })
 
-
-app.get('/time' , function(require ,result){
+app.get('/time' , function(request ,result){
     var date = new Date();
     result.send({status:200, message: date.getHours() + ":" + date.getSeconds()});
 })
+////////////////////End Step#3/////////////////////////
+
+///////////Step#4//////////////////////
+app.get('/hello/:ID' , function(request ,result){
+    var Id = request.params.ID ;
+    result.send({status:200, message: "Hello:" + Id});
+})
+app.get('/search' , function(request ,result){
+    var Search = request.query.s ;
+    if(Search)
+    {
+    result.send({status:200, message: "ok" , data: Search});    
+    }
+    else
+    {
+      result.send({status:500, error:true, message:"you have to provide a search"});
+    }
+})
+
+
